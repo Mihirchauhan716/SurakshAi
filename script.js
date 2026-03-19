f028051dc05a5ad0d6d683f7be64b2fec678dd44
Mihir Chauhan <mihirchauhan716@gmail.com>
2026-03-19 23:45:41 +0530

    corrected the feature card

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-let firstname =prompt("pehla nam daal be")
-let secondname =prompt("last ka nam daal be")
-console.log(`hello $ {firstname } $ {lastname}`)
+
 //day 2 javascript
 // day 3 javascript
-//day 4 javascript
+//day 4 og("Hospital notified!")
+
+
+
+function handleEmergency(location) {
+  console.log(`Emergency detected at ${location}!`)
+  console.log("Finding nearest ambulance...")
+  console.log("Ambulance dispatched!")
+}
+
+// call it with different locations
+handleEmergency("Mumbai Highway")
+handleEmergency("Delhi Metro")
+handleEmergency("Pune Station")
